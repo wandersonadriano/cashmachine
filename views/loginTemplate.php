08dddc2da45abca26be52862073d7ca5d3764a40
Wanderson <wandersonmoraesc@gmail.com>
2021-08-14 16:57:45 -0300

    Adding icons on the side of the fields user and password from the fontawesome.com.

--- views/loginTemplate.php
+++ views/loginTemplate.php
@@ -1,23 +1,28 @@
-<div id="container">
-    <div class="login">
-        <div class="login--bankname">
-            WBank
-        </div>
-        <div class="login--form">
-            <form method="POST" action="models/login.php">
-                <label>    
-                    Conta<br/>
-                    <input type="text" name="account_number" id="field-account_number" />
-                </label>
-                <br/><br/>
-                <label> 
-                    Senha<br/>
-                    <input type="password" name="account_password" id="field-passoword" />
-                </label>
-                <br/><br/>
-                <input type="submit" value="Entrar"/>
-            </form>
+<!DOCTYPE html>
+<div class="loginpage">
+    <div class="container">
+        <div class="loginarea">
+            <div class="loginarea--bankname">
+                WBank
+            </div>
+            <div class="loginarea--form">
+                <form method="POST" action="models/login.php">
+                    <label>    
+                        <i class="fas fa-user" aria-hidden="true" title="Nº da Conta"></i>
+                        <input type="text" name="account_number" id="field-account_number" placeholder="Nº da conta" pattern="[0-9]{10}"/>
+                    </label>
+                    <br/><br/>
+                    <label> 
+                        <i class="fas fa-key" aria-hidden="true" title="Senha"></i>
+                        <input type="password" name="account_password" id="field-passoword" placeholder="Senha"/>
+                    </label>
+                    <br/><br/>
+                    <button type="submit">Entrar</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
+
+<script src="https://kit.fontawesome.com/10b84a3888.js" crossorigin="anonymous"></script>
 
